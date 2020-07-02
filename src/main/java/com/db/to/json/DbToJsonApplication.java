3a940c15f6a9245fab507a8633c11a6847e57ee3
Glen Sequeira <glenvishal@gmail.com>
2020-07-02 10:36:22 +1000

    first commit

---
+++ src/main/java/com/db/to/json/DbToJsonApplication.java
@@ -0,0 +1,13 @@
+package com.db.to.json;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbToJsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbToJsonApplication.class, args);
+	}
+
+}
