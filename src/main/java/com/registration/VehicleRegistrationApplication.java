d5bc725b18fb0d3b755bce53b0fddb996d2bb66f
Glen <glenvishal@gmail.com>
2021-04-17 19:52:04 +1000

    Test commit

--- src/main/java/com/registration/VehicleRegistrationApplication.java
+++ src/main/java/com/registration/VehicleRegistrationApplication.java
@@ -1,6 +1,5 @@
 package com.registration;
 
-//import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
